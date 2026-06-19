1e72b661c90336e73961afa29951259ccb61d8cc
Xuejiao Zhao <xuejiaozhao_snow@foxmail.com>
2026-06-20 00:47:11 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,8 @@ function mailTo(address) {
 function setEmail() {
   // try to prevent spam without annoying users...
   var user = '\u0078\u006A\u007A\u0068\u0061\u006F';  // xjzhao
-  var domain = '\u0040\u006E\u0074\u0075\u002E\u0065\u0064\u0075\u002E\u0073\u0067';  // @ntu.edu.sg
+  // var domain = '\u0040\u006E\u0074\u0075\u002E\u0065\u0064\u0075\u002E\u0073\u0067';  // @ntu.edu.sg
+   var domain = '\u0040\u0073\u0063\u0075\u002E\u0065\u0064\u0075\u002E\u0063\u006E';  // @scu.edu.cn
   $('.my-email').html(mailTo(user + domain));
 }
 
